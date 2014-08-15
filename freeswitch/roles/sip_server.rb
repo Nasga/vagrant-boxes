df88ed5c8689a108484f698912d2f42a0aed2da3
Sébastien ELET <sebastien@elet.fr>
2014-08-15 18:21:53 +0200

    Configure freeswitch modules

--- freeswitch/roles/sip_server.rb
+++ freeswitch/roles/sip_server.rb
@@ -1,5 +1,23 @@
 name "sip_server"
 description "Install a sip server"
+
+override_attributes(
+  "freeswitch" => {
+    "autoload_modules" => %w[
+      mod_console
+      mod_logfile
+      mod_event_socket
+      mod_sofia
+      mod_commands
+      mod_dptools
+      mod_dialplan_xml
+      mod_spidermonkey
+      mod_callcenter
+      mod_xml_cdr
+      mod_xml_curl
+    ]
+  }
+)
 run_list(
   "recipe[apt]",
   "recipe[freeswitch]"
